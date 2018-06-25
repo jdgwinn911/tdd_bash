fc3c6c17789c76ea97e5558b6ec8d13ab4e6d14b
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-25 13:10:17 -0400

    first fix

--- tdd_bash.rb
+++ tdd_bash.rb
@@ -3,6 +3,6 @@ require_relative "bash.rb"
 
 class BashTdd < Minitest::Test
     def test_that_mynum_has_value
-        assert_equal("1234", mynum()[0..5])
+        assert_equal("1", mynum()[0])
     end
 end
