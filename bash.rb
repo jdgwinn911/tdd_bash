ff433afba6120b7059fc154cc9dd097587c3df5a
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-25 14:27:12 -0400

    first fix

--- bash.rb
+++ bash.rb
@@ -1,4 +1,12 @@
 def check_for_match(myticket, winningticket)
-    1
+   myticket = myticket.split('')
+   winningticket = winningticket.split('')
+   matching_num = 0
+   winningticket.each_with_index do |num, index|
+    if num == myticket[index]
+        matching_num += 1
+    end
+end
+matching_num
 end
 
--- tdd_bash.rb
+++ tdd_bash.rb
@@ -10,4 +10,8 @@ class BashTdd < Minitest::Test
     def test_that_1_match_returns_1
         assert_equal(1, check_for_match("1111", "1222"))
     end
+
+    def test_taht_2_matches_returns_2
+        assert_equal(2, check_for_match("1111", "1122"))
+    end
 end
