0c9e82f62b7a29f7fa2241cae343b32ce0dd5f6c
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-27 15:40:51 -0400

    back to normal kinda lol

--- bash.rb
+++ bash.rb
@@ -1,56 +1,40 @@
 def check_for_match(myticket, winningticket)
-   my_ticket = myticket.split('')
-   winning_ticket = winningticket.split('')
-   matching_num = 0
-   winning_ticket.each_with_index do |num, index|
-        unless num == myticket[index]
-            matching_num = matching_num + 1
-        end
-    end
-    matching_num
-end
-
-def check_for_match_arr(myticketarray, winningticket)
-    empty = []
-    myticketarray.each do |num| 
-     check_for_match(num, winningticket)
-     if check_for_match(num, winningticket) == 1
-        empty << num
-     end 
-    end
-    empty
-end
-
-def check_for_match_arr_2(myticketarray, winningticket)
-    empty = []
-    myticketarray.each do |num|
-      empty << check_for_match(num, winningticket)
-    end
-    empty
-end
-
-def winarr(myticketarray, winningticket)
-    warr = []
-    newarr = []
-    winningticket.each do |num|
-        matches = check_for_match_arr_2(myticketarray, num)
-        winners = 0
-        losers = []
-        matches.each do |m|
-            if m == 0
-                winners += 1
-            else  
-                losers << m
-            end
-        end
-        # winners.push losers
-        warr << winners 
-        warr << losers
-        newarr << warr  
-
-    end
-    newarr 
-end
+    my_ticket = myticket.split('')
+    puts "my ticket is #{my_ticket}"
+    winning_ticket = winningticket.split('')
+    puts "winning ticket is #{winning_ticket}"
+    matching_num = 0
+    winning_ticket.each_with_index do |num, index|
+         unless num == myticket[index]
+             matching_num = matching_num + 1
+         end
+     end
+     matching_num
+ end
+ 
+ def check_for_match_arr(myticketarray, winningticket)
+     empty = []
+     myticketarray.each do |num| 
+         empty << check_for_match(num, winningticket)
+     end
+     empty
+ end
+ 
+ def winarr(myticketarray, winningticket)
+     warr = []
+     winningticket.each do |num|
+         matches = check_for_match_arr(myticketarray, num)
+         winners = 0
+         matches.each do |m|
+             if m == 0
+                 winners += 1
+             end
+         end
+         warr << winners
+     end
+     warr 
+ end
+ 
 
 
  
--- tdd_bash.rb
+++ tdd_bash.rb
@@ -15,23 +15,19 @@ class BashTdd < Minitest::Test
         assert_equal(2, check_for_match("1111", "1122"))
     end
 
-    # def test_how_much_tickest_are_off_by
-    #     assert_equal([2,1,0], check_for_match_arr(["1222", "1235", "1234",], "1234"))
-    # end
+    def test_how_much_tickest_are_off_by
+        assert_equal([2,1,0], check_for_match_arr(["1222", "1235", "1234",], "1234"))
+    end
 
-    # def test_how_much_tickest_are_off_by_ver_2
-    #     assert_equal([2, 3, 3, 1], check_for_match_arr(["1146", "1234", "1256", "1119"], "1111"))
-    # end
+    def test_how_much_tickest_are_off_by_ver_2
+        assert_equal([2, 3, 3, 1], check_for_match_arr(["1146", "1234", "1256", "1119"], "1111"))
+    end
 
     def test_if_winners_equal_2
-        assert_equal([[1, [1, 3]], [0, [1, 2, 3]]], winarr(["1234", "1235", "1112"], ["1234", "1244"]))
+        assert_equal([2, 0], winarr(["1234", "1234", "1244", "1255"], ["1234", "1299"]))
     end
 
     def test_if_1_off_return
         assert_equal(["1235"], check_for_match_arr(["1222", "1235", "1234"], "1234"))
-    end
-
-    def test_offbys_are_in_order
-        assert_equal([[1, [1, 3]]], winarr(["1234", "1235", "1112"], ["1234"]))
     end
 end
